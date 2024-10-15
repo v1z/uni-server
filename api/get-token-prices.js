6de3f3135117078ca2687dd2ab7f7e23783013fd
Artem Glukhanko <v1z.workmail@gmail.com>
2024-10-15 18:55:46 +0100

    fix api call

--- api/get-token-prices.js
+++ api/get-token-prices.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
     }
 
     try {
-        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC,ETH', {
+        const response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?id=1,1027', {
             headers: {
                 'X-CMC_PRO_API_KEY': process.env.COINMARKETCAP_API_KEY,
             }
